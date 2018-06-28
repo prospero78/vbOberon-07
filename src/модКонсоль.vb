6fd4241a7616a33daab89c7cd1bb04df8b6b1d11
prospero78su <prospero.78.su@gmail.com>
2018-06-28 21:04:34 +0200

    19 Добавлен модуль цветной консоли

---
+++ src/модКонсоль.vb
@@ -0,0 +1,42 @@
+﻿Public Module модКонсоль ' Цветная консоль компилятора
+
+   Const стрКомпилятор As String = "|   Компилятор Oberon-07   2018 BSD-2  |"
+   Const стрСборка     As String = "|   Сборка 0014     2018-06-28 20:18   |"
+   Const стрАвтор      As String = "|   ООО ""КБК Техника"" (c)              |"
+
+   Public Sub Инфо_Печать(ByRef стрИнфо As String) ' нужен для вывода информационных сообщений белыми буквами на синем фоне
+      Static цветФона As System.ConsoleColor
+      цветФона = Console.BackgroundColor
+      Console.BackgroundColor = System.ConsoleColor.Blue
+      Console.WriteLine(стрИнфо)
+      Console.BackgroundColor = цветФона
+   End Sub
+
+   Public Sub Ошибка_Печать(ByRef стрОшибка As String) ' нужен для вывода сообщений об ошибках белыми буквами на красном фоне
+      Static цветФона As System.ConsoleColor
+      Static цветЛит As System.ConsoleColor
+      цветФона = Console.BackgroundColor
+      цветЛит = Console.ForegroundColor
+      Console.BackgroundColor = System.ConsoleColor.Red
+      Console.ForegroundColor = System.ConsoleColor.Yellow
+      Console.WriteLine(стрОшибка)
+      Console.BackgroundColor = цветФона
+      Console.ForegroundColor = цветЛит
+   End Sub
+
+   Public Sub Сплэш_Печать() 'Первичная надпись при старте компилятора
+      Console.WriteLine()
+      Console.Write("                 ")
+      Инфо_Печать("+------------------------------------+")
+      Console.Write("                 ")
+      Инфо_Печать(стрКомпилятор)
+      Console.Write("                 ")
+      Инфо_Печать(стрСборка)
+      Console.Write("                 ")
+      Инфо_Печать(стрАвтор)
+      Console.Write("                 ")
+      Инфо_Печать("+------------------------------------+")
+      Console.WriteLine()
+   End Sub
+
+   End Module
