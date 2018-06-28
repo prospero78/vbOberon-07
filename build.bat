7522ded36d7d5b932a0ecf95f1e5f32b03dd79b6
prospero78su <prospero.78.su@gmail.com>
2018-06-28 21:01:48 +0200

    15 Исправлены пути в файле сборки

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 echo off
 cls
-chcp 6501
-c:\Windows\Microsoft.NET\Framework64\v4.0.30319\vbc.exe oc.vb
+chcp 65001
+c:\Windows\Microsoft.NET\Framework64\v4.0.30319\vbc.exe .\src\oc.vb
 echo press any key . . .
-move oc.exe ./bin/oc.exe
+move oc.exe .\bin\oc.exe
