90c93051d59fed11fac5f9c4f03c15d389029355
prospero78su <prospero.78.su@gmail.com>
2018-06-28 21:08:52 +0200

    25 Исправлено выходное имя в батнике для сборки

--- src/build_argument.bat
+++ src/build_argument.bat
@@ -1,2 +1,2 @@
 echo ===  Компиляция модАргументы.dll  ===
-c:\Windows\Microsoft.NET\Framework64\v4.0.30319\vbc.exe src\модАргументы.vb /nologo /debug+ /optionexplicit+ /optioninfer+ /optionstrict+ /rootnamespace:oc /platform:x64 /utf8output+ /r:".\bin\модПомощь.dll" /r:".\bin\модКонсоль.dll" /t:library /out:".\bin\модАргументыы.dll" /optimize+
+c:\Windows\Microsoft.NET\Framework64\v4.0.30319\vbc.exe src\модАргументы.vb /nologo /debug+ /optionexplicit+ /optioninfer+ /optionstrict+ /rootnamespace:oc /platform:x64 /utf8output+ /r:".\bin\модПомощь.dll" /r:".\bin\модКонсоль.dll" /t:library /out:".\bin\модАргументы.dll" /optimize+
