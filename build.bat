5346c439330c4056694481d0c86a14cd4f0c92a1
prospero78su <prospero.78.su@gmail.com>
2018-06-28 21:55:26 +0200

    43 Добавление ссылок для сборки главного модуля

--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@ echo off
 chcp 65001   
 cls
 echo ===  Компиляция oc.exe  ===
-c:\Windows\Microsoft.NET\Framework64\v4.0.30319\vbc.exe .\src\oc.vb /nologo /debug+ /optionexplicit+ /optioninfer+ /optionstrict+ /rootnamespace:oc /platform:x64 /utf8output+  /r:".\bin\модКонсоль.dll" /r:".\bin\модАргументы.dll" /t:exe /out:".\bin\oc.exe" /optimize+
+c:\Windows\Microsoft.NET\Framework64\v4.0.30319\vbc.exe .\src\oc.vb /nologo /debug+ /optionexplicit+ /optioninfer+ /optionstrict+ /rootnamespace:oc /platform:x64 /utf8output+  /r:".\bin\модКонсоль.dll" /r:".\bin\модАргументы.dll" /r:".\bin\модФайл.dll" /t:exe /out:".\bin\oc.exe" /optimize+
 
