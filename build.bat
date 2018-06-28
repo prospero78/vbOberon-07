8a6f8f753a56cc5d51cf0969025e3ac98cbcd8da
prospero78su <prospero.78.su@gmail.com>
2018-06-28 21:38:08 +0200

    31 Исправление главного файла сборки

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 echo off
+chcp 65001   
 cls
-chcp 65001
-c:\Windows\Microsoft.NET\Framework64\v4.0.30319\vbc.exe .\src\oc.vb
-echo press any key . . .
-move oc.exe .\bin\oc.exe
+echo ===  Компиляция oc.exe  ===
+c:\Windows\Microsoft.NET\Framework64\v4.0.30319\vbc.exe .\src\oc.vb /nologo /debug+ /optionexplicit+ /optioninfer+ /optionstrict+ /rootnamespace:oc /platform:x64 /utf8output+  /r:".\bin\модКонсоль.dll" /r:".\bin\модАргументы.dll" /t:exe /out:".\bin\oc.exe" /optimize+
+
